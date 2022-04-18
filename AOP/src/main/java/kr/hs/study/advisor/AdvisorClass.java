4a85ac77d054bc7dcc171a4bea1beafe8399245c
0pyaq0 <s2011@e-mirim.hs.kr>
2022-04-18 10:23:06 +0900

    📝 :: AOP around, afterReturning, afterThrowable 메소드

--- AOP/src/main/java/kr/hs/study/advisor/AdvisorClass.java
+++ AOP/src/main/java/kr/hs/study/advisor/AdvisorClass.java
@@ -1,5 +1,7 @@
 package kr.hs.study.advisor;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class AdvisorClass {
 	public void beforeMethod() {
 		System.out.println("Advice의 before method");
@@ -7,5 +9,20 @@ public class AdvisorClass {
 	
 	public void AfterMethod() {
 		System.out.println("Advice의 AfterMethod");
+	}
+	
+	public void aroundmethod(ProceedingJoinPoint pjp) throws Throwable {
+		System.out.println("around 메서드 호출1");
+		pjp.proceed(); // 실제 핵심 메서드 실행
+		System.out.println("around 메서드 호출2");
+	}
+	
+	public void afterReturningMethod(){
+		System.out.println("afterReturningMethod");
+	}
+	
+	public void afterThrowableMethod(Throwable e) {
+		System.out.println("afterThrowableMethod");
+		System.out.println("e : " + e);
 	}
 }
--- AOP/src/main/java/kr/hs/study/beans/TestBean1.java
+++ AOP/src/main/java/kr/hs/study/beans/TestBean1.java
@@ -1,9 +1,12 @@
 package kr.hs.study.beans;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 //targetclass
 public class TestBean1 {
 	public void method1() {
 		System.out.println("TestBean1ÀÇ method1");
+		int a = 100/0;
 	}
 	
 	public void method2() {
