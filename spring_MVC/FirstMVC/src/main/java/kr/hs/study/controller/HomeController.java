7b675f23a94f624fd1f81f6e56225db147e7d88c
심이진 <s2011@e-mirim.hs.kr>
2022-05-30 10:37:13 +0900

    📝 :: Controller 생성

---
+++ spring_MVC/FirstMVC/src/main/java/kr/hs/study/controller/HomeController.java
@@ -0,0 +1,17 @@
+package kr.hs.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/", method=RequestMethod.GET)
+	// GET 방식으로 /로 접속했을 때 아래 home() 함수를 실행하라
+	public String home() {
+		System.out.println("home");
+		return null;
+	}
+	
+}
