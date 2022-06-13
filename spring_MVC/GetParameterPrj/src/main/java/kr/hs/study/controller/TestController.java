42e468b5f63b9f07a0d4e3679d1f73ce0e280e63
심이진 <s2011@e-mirim.hs.kr>
2022-06-13 11:03:14 +0900

    📑 :: WebResult, @PathVariable, @RequestParam

--- spring_MVC/GetParameterPrj/src/main/java/kr/hs/study/controller/TestController.java
+++ spring_MVC/GetParameterPrj/src/main/java/kr/hs/study/controller/TestController.java
@@ -4,7 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class TestController {
@@ -16,6 +19,63 @@ public class TestController {
 		
 		System.out.println("data1 : " + data1);
 		System.out.println("data2 : " + data2);
+		
+		return "result";
+	}
+	
+	@GetMapping("/test2")
+	public String test2(HttpServletRequest request) {
+		String data1 = request.getParameter("data1");
+		String data2 = request.getParameter("data2");
+		String data3[] = request.getParameterValues("data3");
+		
+		System.out.println("data1 : " + data1);
+		System.out.println("data2 : " + data2);
+		
+		for(String str: data3) {
+			System.out.println(str);
+		}
+		return "result";
+	}
+	
+	@GetMapping("/test3")
+	public String test3(WebRequest request) {
+		String data1 = request.getParameter("data1");
+		String data2 = request.getParameter("data2");
+		System.out.println("data1 : " + data1);
+		System.out.println("data2 : " + data2);
+		
+		return "result";
+	}
+	
+	@GetMapping("/test4/{data1}/{data2}/{data3}")
+	public String test4(@PathVariable int data1, @PathVariable int data2, @PathVariable int data3) {
+		System.out.println("data1 : " + data1);
+		System.out.println("data2 : " + data2);
+		System.out.println("data3 : " + data3);
+		
+		return "result";
+	}
+	
+	@GetMapping("/test5")
+	public String test5(@RequestParam int data1, @RequestParam int data2, @RequestParam int data3) {
+		System.out.println("data1 : " + data1);
+		System.out.println("data2 : " + data2);
+		System.out.println("data3 : " + data3);
+		
+		return "result";
+	}
+	
+	@GetMapping("/test6")
+	public String test6(@RequestParam int data1, @RequestParam int data2, @RequestParam int data3[]) {
+
+		System.out.println("data1 : " + data1);
+		System.out.println("data2 : " + data2);
+		
+		for(int str: data3) {
+			System.out.println(str);
+		}
+		
 		
 		return "result";
 	}
--- spring_MVC/GetParameterPrj/src/main/webapp/WEB-INF/views/index.jsp
+++ spring_MVC/GetParameterPrj/src/main/webapp/WEB-INF/views/index.jsp
@@ -8,6 +8,11 @@
 </head>
 <body>
 <h1>hi Spring MVC~</h1>
-<a href="test1?data1=100&data2=200"/><br> /
+<a href="test1?data1=100&data2=200"/><br/>
+<a href="test2?data1=100&data2=200&data3=300&data3=400">test2 get</a><br/>
+<a href="test3?data1=500&data2=600">test3 WebRequest</a><br/>
+<a href="test4/700/800/900">test4 @PathVariable</a><br/>
+<a href="test5?data1=100&data2=200&data3=300&data3=400">test5 @RequestParam ∞°¿Â ∏π¿Ã æ∏</a><br/>
+<a href="test6?data1=40&data2=50&data3=60&data3=70">test6 @RequestParam array</a><br/>
 </body>
 </html>
