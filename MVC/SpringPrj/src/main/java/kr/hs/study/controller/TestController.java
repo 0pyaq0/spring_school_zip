9e129266faab027ae6e11821f7eb7129e530a265
심이진 <s2011@e-mirim.hs.kr>
2022-06-30 14:10:29 +0900

    📝 :: 구구단 설정

--- MVC/SpringPrj/src/main/java/kr/hs/study/controller/TestController.java
+++ MVC/SpringPrj/src/main/java/kr/hs/study/controller/TestController.java
@@ -3,17 +3,34 @@ package kr.hs.study.controller;
 import java.util.Scanner;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestController {
 	
-	//1. 1부터 10까지의 합
-	@GetMapping
-	public void result() {
-		Scanner sc = new Scanner(System.in);
-		int dan = sc.nextInt();
-		System.out.println("단을 입력하시오 : " + dan);
+	@GetMapping("/sum")
+	public String sum(Model model) {
+		int result = 0;
+		for(int i=1; i<=10; i++) {
+			result += i;
+		}
+		model.addAttribute("sum", result);
+		return "sum";
+	}
+	
+	@GetMapping("/multi_table")
+	public String multiTable() {
+		return "multi_table";
+	}
+	
+	@GetMapping("/multi")
+	public String multi(@RequestParam int number) {
+		for(int i=1; i<=9; i++) {
+			System.out.println(i + " * " + number + " = " + (i*number));
+		}
+		return "result";
 	}
 }
